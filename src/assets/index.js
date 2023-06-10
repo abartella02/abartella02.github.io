2b614b6c494cc0a073aba4c0c304eaaa78690e59
bartella <bartella@mcmaster.ca>
2023-06-09 23:01:11 -0400

    reorganized imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,16 +9,12 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
 import python from "./tech/python.png";
@@ -34,6 +30,11 @@ import selenium from "./tech/selenium.png";
 import vba from "./tech/vba.png";
 import verilog from "./tech/verilog.png";
 import pytest from "./tech/pytest.png";
+
+import typescript from "./tech/typescript.png";
+import redux from "./tech/redux.png";
+import mongodb from "./tech/mongodb.png";
+import figma from "./tech/figma.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -55,25 +56,13 @@ export {
   close,
   css,
   docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
-
   assembly, 
   bash, 
   clang, 
@@ -87,4 +76,18 @@ export {
   vba,
   verilog,
   pytest,
+
+  figma,
+  mongodb,
+  redux,
+  typescript,
+
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+
+  carrent,
+  jobit,
+  tripguide,
 };
